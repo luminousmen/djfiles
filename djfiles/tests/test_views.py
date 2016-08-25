841a9a9bfea0fa565edd294b6764906a1a917e5c
Kirill Bobrov <miaplanedo@mail.ru>
2016-08-25 17:12:14 +0300

    delete get_file bad test

--- djfiles/tests/test_views.py
+++ djfiles/tests/test_views.py
@@ -17,22 +17,13 @@ class ViewTestCase(TestCase):
 		self.f.write(str("file_content").encode('utf-8'))
 		self.f = open("test.txt", 'rb')
 
-		f = SimpleUploadedFile("test.txt", self.f.read() )
+		f = SimpleUploadedFile("test.txt", self.f.read())
 		self.with_file = File(slug='with_file', content=f)
 		self.with_file.save()
-
 
 	def test_no_file(self):
 		# no file assosiated with instance
 		self.assertRaises(ValueError, get_file, None, 'no_file')
-		
-
-	def test_with_file(self):
-		# test getting not empty file
-		response = get_file(None, 'with_file')
-		content = "attachment; filename={} - {}".format(self.with_file.content, self.with_file.slug)
-
-		self.assertEqual(response['Content-Disposition'], content)
 
 	def tearDown(self):
 		self.no_file.delete()
